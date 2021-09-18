5072cacf5732e9d73f3d9969a1e4e6a77f1f43b4
Ravi Teja Gattu <46156268+rtlyf217@users.noreply.github.com>
2021-09-18 22:12:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import re 
 
-
 # Question 1: -----------------------------------
 regex = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w+$' 
-
-
+      
 def check_email(email):  
     if(re.search(regex,email)):  
         return True          
@@ -19,16 +17,18 @@ def check_psw_digit(psw):
   result = any(char.isdigit() for char in psw)
   return result
 
+# This function should check if there is any lower case letter so the change to be made is isupper() --> islower()
 def check_psw_lower(psw):
-  result = any(char.isupper() for char in psw)
+  result = any(char.islower() for char in psw)
   return result
 
+# Password must be atleast 8 characters so >= 7 should be changed to > 7.
 def check_psw(psw):
   if(check_psw_digit(psw)
             and check_psw_upper(psw)
             and check_psw_lower(psw)
             and check_psw_digit(psw)
-            and len(psw) >= 7):
+            and len(psw) > 7):
     print("Valid password")
     return True
   else:
@@ -41,4 +41,3 @@ def check_psw_equal(psw1, psw2):
 
 def check_credentials(email, psw1, psw2):
   return check_email(email) and check_psw(psw1) and check_psw_equal(psw1, psw2)
-
